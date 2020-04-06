f3b35b13eafcf944c24dbdc6df589cb3811ad48d
pschaus <pschaus@pschaus-MacBook-Pro.local>
2020-04-06 15:45:29 +0200

    hospi per case in provinces

--- app.py
+++ app.py
@@ -39,21 +39,40 @@ map_communes = px.choropleth_mapbox(df_communes_tot, geojson=geojson_communes,
 
 with open('static/json/be-provinces-geojson.json') as json_file:
     geojson_provinces = json.load(json_file)
+range_min = df_prov_tot.CASES_PER_THOUSAND.min()
+range_max = df_prov_tot.CASES_PER_THOUSAND.max()
 map_provinces = px.choropleth_mapbox(df_prov_tot, geojson=geojson_provinces,
                                      locations="PROVINCE",
                                      color='CASES_PER_THOUSAND', color_continuous_scale="Viridis",
-                                     range_color=(0, 2.8),
+                                     range_color=(range_min, range_max),
                                      featureidkey="properties.proviso",
                                      center={"lat": 50.85045, "lon": 4.34878},
                                      hover_name="CASES_PER_THOUSAND",
                                      height=900,
                                      mapbox_style="carto-positron", zoom=7)
 
+
+# ---------plot of cases per province------------------------
+
+with open('static/json/be-provinces-geojson.json') as json_file:
+    geojson_provinces = json.load(json_file)
+range_min = df_prov_tot.HOSPI_PER_CASES.min()
+range_max = df_prov_tot.HOSPI_PER_CASES.max()
+map_provinces_hospi_per_cases = px.choropleth_mapbox(df_prov_tot, geojson=geojson_provinces,
+                                     locations="PROVINCE",
+                                     color='HOSPI_PER_CASES', color_continuous_scale="Viridis",
+                                     range_color=(range_min,range_max),
+                                     featureidkey="properties.proviso",
+                                     center={"lat": 50.85045, "lon": 4.34878},
+                                     hover_name="HOSPI_PER_CASES",
+                                     height=900,
+                                     mapbox_style="carto-positron", zoom=7)
+
+
 # ---------bar plot cases time series per commune-----------------
 def barplot(commune_nis = 73006):
     [nis, case, fr, nl] = df_communes_tot.loc[df_communes_tot['NIS5'] == str(commune_nis)].values[0]
     title = title_text = fr + " / " + nl
-    print(title)
     fig = go.Figure([go.Bar(x=df_communes_timeseries['DATE'], y=df_communes_timeseries[str(commune_nis)], text='cases')])
     fig.update_layout(title_text=title)
     return fig
@@ -116,8 +135,6 @@ for ag in age_groups:
     df_ag = df_ag.groupby(['DATE']).agg({'DEATHS': 'sum'})
     df_ag.index = pd.DatetimeIndex(df_ag.index)
     df_ag = df_ag.reindex(idx, fill_value=0)
-    print(df_ag.index)
-    print(df_ag['DEATHS'])
     bars_age_groups_deaths.append(go.Bar(
         x=df_ag.index,
         y=df_ag['DEATHS'],
@@ -148,6 +165,12 @@ app.layout = html.Div(children=[
     dbc.Row(
         [
             dbc.Col(dcc.Graph(id='map-prov', figure=map_provinces), ),
+
+        ]
+    ),
+    dbc.Row(
+        [
+            dbc.Col(dcc.Graph(id='map_provinces_hospi_per_cases', figure=map_provinces_hospi_per_cases), ),
 
         ]
     ),
