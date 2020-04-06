063dd3e4d23f3488ad05ad1b8a3c7ae62b1c8d6f
pschaus <pschaus@pschaus-MacBook-Pro.local>
2020-04-06 15:45:29 +0200

    hospi per case in provinces

--- scripts/covid-provinces.py
+++ scripts/covid-provinces.py
@@ -35,33 +35,56 @@ prov_population = {"VBR":1129849,
 
 url="https://epistat.sciensano.be/Data/COVID19BE_CASES_AGESEX.csv"
 s=requests.get(url).content
-df=pd.read_csv(io.StringIO(s.decode('latin-1'))) # last line is NaN
+df_prov=pd.read_csv(io.StringIO(s.decode('latin-1'))) # last line is NaN
 
 # MAP PROVINCE CODES
-df['PROVINCE_NAME']= df['PROVINCE']
-df['PROVINCE']= df['PROVINCE'].map(prov_codes)
+df_prov['PROVINCE_NAME']= df_prov['PROVINCE']
+df_prov['PROVINCE']= df_prov['PROVINCE'].map(prov_codes)
+
+# Filter NaN (yes the data is not totally clean)
+df_prov.dropna(inplace=True)
+df_prov.DATE = pd.to_datetime(df_prov.DATE)
+
+df_prov.to_csv('../static/csv/be-covid-provinces.csv', index=True)
 
 
+# ----------------------------------------------------
+
+
+
+url="https://epistat.sciensano.be/Data/COVID19BE_HOSP.csv"
+s=requests.get(url).content
+df_hospi=pd.read_csv(io.StringIO(s.decode('latin-1'))) # last line is NaN
+
+# MAP PROVINCE CODES
+df_hospi['PROVINCE_NAME']= df_hospi['PROVINCE']
+df_hospi['PROVINCE']= df_hospi['PROVINCE'].map(prov_codes)
 
 
 # Filter NaN (yes the data is not totally clean)
-df.dropna(inplace=True)
-df.DATE = pd.to_datetime(df.DATE)
+df_hospi.dropna(inplace=True)
+df_hospi.DATE = pd.to_datetime(df_hospi.DATE)
 
-df.to_csv('../static/csv/be-covid-provinces.csv',index=True)
+df_hospi.to_csv('../static/csv/be-covid-hospi.csv', index=True)
 
 
-# total number per provinces
-df_tot_provinces = df.groupby('PROVINCE').agg({'CASES': 'sum'})
+# -----------------------------------------------------------------
+
+
+# total number of cases per provinces
+df_tot_provinces = df_prov.groupby('PROVINCE').agg({'CASES': 'sum'})
 df_tot_provinces["POPULATION"] = df_tot_provinces.index.map(mapper=prov_population)
 df_tot_provinces["CASES_PER_THOUSAND"] = df_tot_provinces["CASES"]*1000/df_tot_provinces["POPULATION"]
+
+df_tot_provinces_hospi = df_hospi.groupby('PROVINCE').agg({'NEW_IN': 'sum'})
+
+df_tot_provinces["NEW_IN"] = df_tot_provinces_hospi['NEW_IN']
+
+df_tot_provinces["NEW_IN_PER_CASES"] = df_tot_provinces_hospi['NEW_IN']/df_tot_provinces["CASES"] #nombre d'hospi/nombre de cas
 df_tot_provinces.to_csv('../static/csv/be-covid-provinces_tot.csv',index=True)
 
 
-
-
-
-
+print(df_tot_provinces)
 
 
 
