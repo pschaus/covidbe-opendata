8498b6dcd317d5845ca95f478234007f10ac490c
pschaus <pschaus@pschaus-MacBook-Pro.local>
2020-04-06 10:03:47 +0200

    refactor app

--- app.py
+++ app.py
@@ -12,52 +12,62 @@ import plotly.express as px
 
 import pandas as pd
 
-df = pd.read_csv("static/csv/be-covid-totcases.csv", dtype={"NIS5": str})
+df_communes_tot = pd.read_csv("static/csv/be-covid-totcases.csv", dtype={"NIS5": str})
+df_communes_timeseries = pd.read_csv('static/csv/be-covid-timeseries.csv')
+df_prov_tot = pd.read_csv('static/csv/be-covid-provinces_tot.csv')
+df_prov_timeseries = pd.read_csv('static/csv/be-covid-provinces.csv')
 
-dft = pd.read_csv('static/csv/be-covid-timeseries.csv')
 
-df_prov = pd.read_csv('static/csv/be-covid-provinces_tot.csv')
-
+# ------------plot of cases per communes-----------------------
 
 with open('static/json/be-geojson.json') as json_file:
     geojson_communes = json.load(json_file)
+map_communes = px.choropleth_mapbox(df_communes_tot, geojson=geojson_communes,
+                                    locations="NIS5",
+                                    color='CASES', color_continuous_scale="Viridis",
+                                    range_color=(0, 300),
+                                    featureidkey="properties.shn",
+                                    center={"lat": 50.85045, "lon": 4.34878},
+                                    hover_name="CASES",
+                                    hover_data=["FR", "NL"],
+                                    custom_data=["NIS5"],
+                                    height=900,
+                                    mapbox_style="carto-positron", zoom=7)
 
-map_cases = px.choropleth_mapbox(df, geojson=geojson_communes,
-                                 locations="NIS5",
-                                 color='CASES', color_continuous_scale="Viridis",
-                                 range_color=(0, 300),
-                                 featureidkey="properties.shn",
-                                 center={"lat": 50.85045, "lon": 4.34878},
-                                 hover_name="CASES",
-                                 hover_data=["FR", "NL"],
-                                 custom_data=["NIS5"],
-                                 height=900,
-                                 mapbox_style="carto-positron", zoom=7)
 
+# ---------plot of cases per province------------------------
 
 with open('static/json/be-provinces-geojson.json') as json_file:
     geojson_provinces = json.load(json_file)
+map_provinces = px.choropleth_mapbox(df_prov_tot, geojson=geojson_provinces,
+                                     locations="PROVINCE",
+                                     color='CASES_PER_THOUSAND', color_continuous_scale="Viridis",
+                                     range_color=(0, 2.8),
+                                     featureidkey="properties.proviso",
+                                     center={"lat": 50.85045, "lon": 4.34878},
+                                     hover_name="CASES_PER_THOUSAND",
+                                     height=900,
+                                     mapbox_style="carto-positron", zoom=7)
 
-map_provinces = px.choropleth_mapbox(df_prov, geojson=geojson_provinces,
-                                 locations="PROVINCE",
-                                 color='CASES_PER_THOUSAND', color_continuous_scale="Viridis",
-                                 range_color=(0, 2.8),
-                                 featureidkey="properties.proviso",
-                                 center={"lat": 50.85045, "lon": 4.34878},
-                                 hover_name="CASES_PER_THOUSAND",
-                                 height=900,
-                                 mapbox_style="carto-positron", zoom=7)
+# ---------bar plot cases time series per commune-----------------
+def barplot(commune_nis = 73006):
+    [nis, case, fr, nl] = df_communes_tot.loc[df_communes_tot['NIS5'] == str(commune_nis)].values[0]
+    title = title_text = fr + " / " + nl
+    print(title)
+    fig = go.Figure([go.Bar(x=df_communes_timeseries['DATE'], y=df_communes_timeseries[str(commune_nis)], text='cases')])
+    fig.update_layout(title_text=title)
+    return fig
 
+barplot_communes = barplot()
 
+# ---------bar plot age groups---------------------------
 
-df_prov_all = pd.read_csv('static/csv/be-covid-provinces.csv')
-idx = pd.date_range(df_prov_all.DATE.min(), df_prov_all.DATE.max())
-
+idx = pd.date_range(df_prov_timeseries.DATE.min(), df_prov_timeseries.DATE.max())
 # bar plot with bars per age groups
 bars_age_groups = []
-age_groups = sorted(df_prov_all.AGEGROUP.unique())
+age_groups = sorted(df_prov_timeseries.AGEGROUP.unique())
 for ag in age_groups:
-    df_ag = df_prov_all.loc[df_prov_all['AGEGROUP'] == ag]
+    df_ag = df_prov_timeseries.loc[df_prov_timeseries['AGEGROUP'] == ag]
     df_ag = df_ag.groupby(['DATE']).agg({'CASES': 'sum'})
     df_ag.index = pd.DatetimeIndex(df_ag.index)
     df_ag = df_ag.reindex(idx, fill_value=0)
@@ -71,6 +81,9 @@ fig_age_groups = go.Figure(data=bars_age_groups,
 fig_age_groups.update_layout(height=1000,)
 
 
+# ---------------------------------------------
+
+
 app = dash.Dash(__name__)
 server = app.server
 
@@ -80,8 +93,8 @@ app.layout = html.Div(children=[
     html.H2(children='COVID-DATA',id='my-hoverdata'),
     dbc.Row(
         [
-            dbc.Col(dcc.Graph(id='map-communes', figure=map_cases),),
-            dbc.Col(dcc.Graph(id='histogram', figure=map_cases),),
+            dbc.Col(dcc.Graph(id='map-communes', figure=map_communes), ),
+            dbc.Col(dcc.Graph(id='histogram', figure=barplot_communes), ),
 
         ]
     ),
@@ -101,6 +114,9 @@ app.layout = html.Div(children=[
 ])
 
 
+# ---------------callbacks-----------------------
+
+
 @app.callback(
 Output('my-hoverdata', 'children'),
 [Input('map-communes', 'hoverData')])
@@ -109,10 +125,10 @@ def callback_image(hoverData):
         return "place your mouse on a municipality"
     else:
         idx = hoverData["points"][0]["pointIndex"]
-        if df.iloc[[idx]]['FR'].item() != df.iloc[[idx]]['NL'].item():
-            return df.iloc[[idx]]['FR']+"     "+df.iloc[[idx]]['NL']+"    cases:"+ str(df.iloc[[idx]]['CASES'].item())
+        if df_communes_tot.iloc[[idx]]['FR'].item() != df_communes_tot.iloc[[idx]]['NL'].item():
+            return df_communes_tot.iloc[[idx]]['FR'] + "     " + df_communes_tot.iloc[[idx]]['NL'] + "    cases:" + str(df_communes_tot.iloc[[idx]]['CASES'].item())
         else:
-            return df.iloc[[idx]]['FR'] + "    cases:"+ str(df.iloc[[idx]]['CASES'].item())
+            return df_communes_tot.iloc[[idx]]['FR'] + "    cases:" + str(df_communes_tot.iloc[[idx]]['CASES'].item())
 
 
 # Update Histogram Figure based on Month, Day and Times Chosen
@@ -121,13 +137,9 @@ def callback_image(hoverData):
     [Input('map-communes', 'clickData')])
 def callback_barplot(clickData):
     if clickData == None:
-         return go.Figure([go.Bar(x=dft['DATE'], y=dft[str(73006)])])
-    custom = clickData['points'][0]['customdata']
-    [nis,fr,nl] = custom
-    title = title_text=fr+" / "+nl
-    fig = go.Figure([go.Bar(x=dft['DATE'], y=dft[str(nis)], text ='cases')])
-    fig.update_layout(title_text=title)
-    return fig
+         return barplot()
+    nis = clickData['points'][0]['customdata'][0]
+    return barplot(commune_nis=nis)
 
 
 
