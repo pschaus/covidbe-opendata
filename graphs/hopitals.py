b9d91ad9faf3b9892b8406826856b0ff675ef85d
pschaus <pschaus@pschaus-MacBook-Pro.local>
2020-04-11 08:10:08 +0200

    hospi per case per province

--- graphs/hopitals.py
+++ graphs/hopitals.py
@@ -31,3 +31,42 @@ def bar_hospitalization():
                             yaxis_title='Number of / Day')
 
     return fig_hospi
+
+
+
+
+
+
+df_prov = pd.read_csv('static/csv/be-covid-provinces.csv')
+
+# compute number of hospi / case in each province
+df_tot_provinces_hospi = df_hospi.groupby(['PROVINCE_NAME']).agg({'NEW_IN': 'sum'})
+df_tot_provinces_cases = df_prov.groupby(['PROVINCE_NAME']).agg({'CASES': 'sum'})
+df_tot_provinces_hospi["NEW_IN_PER_CASES"] = df_tot_provinces_hospi['NEW_IN']/df_tot_provinces_cases["CASES"] #nombre d'hospi/nombre de cas
+df_tot_provinces_hospi.reset_index(level=0, inplace=True)
+df_tot_provinces_hospi.sort_values(by=["NEW_IN_PER_CASES"],inplace=True)
+range_min = df_tot_provinces_hospi["NEW_IN_PER_CASES"].min()
+range_max = df_tot_provinces_hospi["NEW_IN_PER_CASES"].max()
+
+
+
+def bar_hospi_per_case_per_province():
+    fig = px.bar(df_tot_provinces_hospi,
+                 y='PROVINCE_NAME',
+                 x='NEW_IN_PER_CASES',
+                 color='NEW_IN_PER_CASES',
+                 orientation='h',
+                 color_continuous_scale="deep",
+                 range_color=(range_min, range_max),
+                 hover_name="PROVINCE_NAME",
+                 labels={'NEW_IN_PER_CASES': 'Total Hospitalisation per positive case'},
+                 height=400)
+    fig.update_traces(
+        hovertemplate=gettext("<b>%{y}</b><br>%{x:.3f} hospitalisation per postive case"),
+        textposition='outside',
+        texttemplate='%{x:.3f}'
+    )
+    fig.layout.coloraxis.colorbar.titleside = 'right'
+    fig.layout.yaxis.title = ""
+    fig.update_layout(template="plotly_white", margin=dict(l=0, r=0, t=5, b=0))
+    return fig
---
+++ pages/hospitals/hospitals_prov.py
@@ -0,0 +1,18 @@
+import dash
+import dash_bootstrap_components as dbc
+import dash_core_components as dcc
+import dash_html_components as html
+from flask_babel import get_locale, gettext
+
+from graphs.hopitals import bar_hospi_per_case_per_province
+
+
+def display_hospitals_prov():
+    return [
+        html.H2(gettext("Hospitalization Per Provinces")),
+        dbc.Row([
+            dbc.Col(dcc.Graph(id='hospitalization-prov',
+                              figure=bar_hospi_per_case_per_province(),
+                              config=dict(locale=str(get_locale())))),
+        ]),
+    ]
