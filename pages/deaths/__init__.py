5f914e713bd51b841ee857fcaaa4dfaa24b1598b
Guillaume Derval <guillaume@guillaumederval.be>
2020-04-13 19:59:39 +0200

    Add plot in tw/fb

--- pages/deaths/__init__.py
+++ pages/deaths/__init__.py
@@ -1,5 +1,7 @@
 from flask_babel import lazy_gettext
 
+from graphs.obituary import rolling_ratio_plot
+from graphs.overmortality import overmortality_estimates_repartition_bar
 from pages import AppMenu, AppLink
 
 from pages.deaths.overmortality import display_overmortality
@@ -8,6 +10,8 @@ from pages.deaths.obituary import display_obituary
 
 deaths_menu = AppMenu(lazy_gettext("Deaths"), "/deaths", [
     AppLink(lazy_gettext("Death per age group"), lazy_gettext("Age Groups"), "/age_groups", display_age_groups),
-    AppLink(lazy_gettext("Overmortality"), lazy_gettext("Overmortality"), "/overmortality", display_overmortality),
-    AppLink(lazy_gettext("Obituary evolution"), lazy_gettext("Obituary evolution"), "/obituary", display_obituary),
+    AppLink(lazy_gettext("Overmortality"), lazy_gettext("Overmortality"), "/overmortality", display_overmortality,
+            plot=overmortality_estimates_repartition_bar),
+    AppLink(lazy_gettext("Obituary evolution"), lazy_gettext("Obituary evolution"), "/obituary", display_obituary,
+            plot=rolling_ratio_plot),
 ])
