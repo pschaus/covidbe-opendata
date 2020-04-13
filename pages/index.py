f7221808f573219ace2548ff016bcbee3e56db30
pschaus <pschaus@pschaus-MacBook-Pro.local>
2020-04-13 11:16:52 +0200

    about + covid licence

--- pages/index.py
+++ pages/index.py
@@ -16,33 +16,57 @@ def display_index():
                 dcc.Markdown("""
                                  We gather data about the Coronavirus in Belgium, and attempt to extract meaningful information from it.
     
-                                 Each menu on the left (or above the page if you are on a phone) shows a different analysis.""")
+                                 Each menu on the left (or above the page if you are on a phone) shows a different analysis.
+                                 
+                                 
+                                 Most of our visualization are interactive, use your mouse and fingers to play with it.
+                                 """)
             ], className="index_first")
         ]),
         html.H3(["Open Data"]),
         dcc.Markdown("""
                 We use open and/or public data to enable these analyses. Our sources are indicated below each analysis.
-
-                The data we gather is available on our [Github repository](https://github.com/pschaus/covidbe-opendata),
-                along with the software needed to produce it. Feel free to reuse!
+                
+                
+                All the source code [Github repository](https://github.com/pschaus/covidbe-opendata), graphics and data
+                we produce are available under the [OPEN COVID LICENSE 1.0](https://opencovidpledge.org/license/v1-0/).
+                In short, feel free to use them without any restriction.
+                
             """),
         html.H3(["Contributing to the project"]),
         dcc.Markdown("""
-                Whether you are a doctor, a data analysis specialist, or a member of the general public, you 
+                Whether you are a doctor, epidemiologist,  a data analysis specialist, or a member of the general public, you 
                 may have
                 - questions that we may be able to answer with more data, 
                 - some data that we could use (and open to the public!),
-                - ideas of new analysis.
+                - ideas of new analysis that we could do.
+                
+                We are also working on a survey project to collect data to prepare the lift of the lockdown. If you are a doctor or epidemiologist and want
+                to help us in the design of this survey, please contact us. 
 
-                If this is the case, don't hesitate to contact us by mail (see the [About page](/about)) or to go directly
-                to our [Github page](https://github.com/pschaus/covidbe-opendata). 
-                We gladly accept new contributions!
+                You can reach us by email (see the [About page](/about)) or twitter [@Covidatabe](https://twitter.com/Covidatabe).
+                We gladly accept new contributions as pull-requests [Github repository](https://github.com/pschaus/covidbe-opendata) !
             """)
     ]
 
 
 def display_about():
-    return html.H1([html.Img(src='/assets/covidata.png'), "Covidata.be"], className="logo_index")
+    return [html.H1([html.Img(src='/assets/covidata.png'), "Covidata.be"], className="logo_index"),
+            dcc.Markdown("""
+            We are a team specialized in [AI, Analytics](https://aia.info.ucl.ac.be/people/) and [Computer Science](https://uclouvain.be/fr/instituts-recherche/icteam/ingi) from @UCLouvain_be and @EPL_ECLouvain.
+            We produce this website with the hope it can help you to better understand and objectively analyze the impact of the COVID crisis on Belgium and his sub-regions.
+            
+            The current main contributors to this project are:
+ 
+            - [Guillaume Derval, UCLouvain](https://www.linkedin.com/in/guillaumederval/)
+            - [Pierre Schaus, UCLouvain](https://www.info.ucl.ac.be/~pschaus)
+            - [Vincent François, UCLouvain](http://vincent.francois-l.be)
+            
+            """)
+    ]
+
+
+
 
 
 index_menu = AppMenu('index', '', [
