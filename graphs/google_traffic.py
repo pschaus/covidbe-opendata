c918e8b7b704f9b2261cd61ef4eca236b15bf273
Pierre Schaus <pschaus@gmail.com>
2020-04-19 07:50:07 +0200

    update countries

--- graphs/google_traffic.py
+++ graphs/google_traffic.py
@@ -8,16 +8,21 @@ from graphs import register_plot_for_embedding
 
 start = '03-01'
 
-df = pd.read_csv(f'static/csv/MAPS.csv', header=None, names=["date", "value"])
-mask = (df['date'] >= f'{2020}-{start}')
-df = df.loc[mask]
-df['date'] = pd.to_datetime(df['date'])
-df.reset_index(level=0, inplace=True)
-df.set_index('date', inplace=True)
-df = df.between_time('08:00', '08:00')
-df = df[df.index.dayofweek < 5]
-df.reset_index(level=0, inplace=True)
 
+countries = ['BE','FR','DE','NL']
+
+def get_df(country):
+    df = pd.read_csv(f'static/csv/traffic/{country}/MAPS.csv',parse_dates = ['date'])
+    df.reset_index(level=0, inplace=True)
+    df.set_index('date', inplace=True)
+    df = df.between_time('08:00', '08:00')
+    df = df[df.index.dayofweek < 5]
+    df.reset_index(level=0, inplace=True)
+    tot = df['value'].sum()
+    df['value'] =df['value']/tot
+    return df
+
+dfmap = {c:get_df(c) for c in countries}
 
 
 
@@ -26,10 +31,16 @@ def plot_google_traffic_working_days():
     """
     google traffic working days
     """
+    fig = go.Figure()
+    for c in countries:
+        df = dfmap[c]
+        fig.add_trace(go.Scatter(x=df.date, y=df.value, mode='lines', name=c))
 
-    fig = px.scatter(df, x='date', y='value',
-                     labels={'date': 'date', 'value': 'google MAP traffic BE working days 8:00AM'})
-
+    fig.update_layout(
+        title="Normalized google map traffic 8:00-8:30 workdays",
+        xaxis_title="day",
+        yaxis_title="normalized traffic"
+    )
     return fig
 
 
