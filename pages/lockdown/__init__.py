748b3afe41f095108398aeb0dbc50f9eadc3f896
Pierre Schaus <pschaus@gmail.com>
2020-04-20 11:53:30 +0200

    update graph google mobility

--- pages/lockdown/__init__.py
+++ pages/lockdown/__init__.py
@@ -1,9 +1,11 @@
 from flask_babel import lazy_gettext
 
 from pages import AppMenu, AppLink
-from pages.lockdown.mobility import display_mobility
+from pages.lockdown.traffic import display_traffic
+from pages.lockdown.mobility_report import display_mobility
 
 
 lockdown_menu = AppMenu(lazy_gettext("Lockdown Impact"), "/lockdown", [
-    AppLink(lazy_gettext("Mobility"), lazy_gettext("Mobility"), "/mobility", display_mobility),
+    AppLink(lazy_gettext("Google-Map Traffic"), lazy_gettext("Google Map Internet Traffic EU"), "/mobility", display_traffic),
+    AppLink(lazy_gettext("Google Mobility Report"), lazy_gettext("Google Mobility Report EU"), "/google-mobility", display_mobility),
 ])
--- pages/lockdown/traffic.py
+++ pages/lockdown/traffic.py
@@ -5,11 +5,9 @@ import dash_html_components as html
 from flask_babel import get_locale, gettext
 
 from graphs.google_traffic import plot_google_traffic_working_days
-from graphs.google_mobility import mobility_plot_eu
-from pages.sources import display_source_providers, source_google_traffic, source_google_mobility
+from pages.sources import display_source_providers, source_google_traffic
 
-
-def display_mobility():
+def display_traffic():
     return [
         html.H2(gettext("Google Map Usage working days 8:00-8:30")),
         dbc.Row([
@@ -18,13 +16,4 @@ def display_mobility():
                               config=dict(locale=str(get_locale())))),
         ]),
         display_source_providers(source_google_traffic),
-
-
-        html.H2(gettext("Google Mobility Report Europe")),
-        dbc.Row([
-            dbc.Col(dcc.Graph(id='google mobility report eu',
-                              figure=mobility_plot_eu(),
-                              config=dict(locale=str(get_locale())))),
-        ]),
-        display_source_providers(source_google_mobility)
     ]
