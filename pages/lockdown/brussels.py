7d2e66c7f911071576e8507ab28826b22c6b2b5a
Pierre Schaus <pschaus@gmail.com>
2020-04-24 09:27:04 +0200

    brussels mobility relative

--- pages/lockdown/brussels.py
+++ pages/lockdown/brussels.py
@@ -4,17 +4,24 @@ import dash_core_components as dcc
 import dash_html_components as html
 from flask_babel import get_locale, gettext
 
-from graphs.brussels import brussels_tunnels
+from graphs.brussels import brussels_tunnels, brussels_tunnels_ratio
 from pages.sources import display_source_providers, source_google_traffic, source_brussels_mobility
 from pages import get_translation
 
 def display_brussels():
     return [
-        html.H2(gettext("Brussels tunnel traffic working days 7:00-8:00")),
+        html.H2(gettext("Brussels tunnels traffic working days 7:00-8:00")),
         dbc.Row([
             dbc.Col(dcc.Graph(id='brussels-tunnel',
                               figure=brussels_tunnels(),
                               config=dict(locale=str(get_locale())))),
         ]),
+        html.H2(gettext("Brussels tunnels relative traffic working days 7:00-8:00")),
+        dbc.Row([
+            dbc.Col(dcc.Graph(id='brussels-tunnel-ratio',
+                              figure=brussels_tunnels_ratio(),
+                              config=dict(locale=str(get_locale())))),
+        ]),
         display_source_providers(source_brussels_mobility),
+
     ]
