ccec392d21dd70797ed4c0436280f7699e0a17dc
Pierre Schaus <pschaus@gmail.com>
2020-04-24 08:48:40 +0200

    brussels tunnels

--- pages/lockdown/__init__.py
+++ pages/lockdown/__init__.py
@@ -3,9 +3,11 @@ from flask_babel import lazy_gettext
 from pages import AppMenu, AppLink
 from pages.lockdown.traffic import display_traffic
 from pages.lockdown.mobility_report import display_mobility
+from pages.lockdown.brussels import display_brussels
 
 
 lockdown_menu = AppMenu(lazy_gettext("Lockdown Impact"), "/lockdown", [
     AppLink(lazy_gettext("Mobility Reports"), lazy_gettext("Mobility Report"), "/mobility-reports", display_mobility),
     AppLink(lazy_gettext("Google-Map Traffic"), lazy_gettext("Google Map Internet Traffic EU"), "/google-map", display_traffic),
+    AppLink(lazy_gettext("Brussels"), lazy_gettext("Brussels"), "/brussels", display_brussels),
 ])
