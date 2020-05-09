5278629b3ab67f08c64e0799ce93b487f66d4f5d
Pierre Schaus <pschaus@gmail.com>
2020-05-09 08:32:08 +0200

    update

--- pages/international/__init__.py
+++ pages/international/__init__.py
@@ -7,7 +7,7 @@ from pages.international.cases_per_million import display_cases_per_million, cal
 from pages.international.deaths_per_million import display_deaths_per_million, callback_deaths_per_million
 
 international_menu = AppMenu("International", "/international", [
-    AppLink("EUROMOMO", "EUROMOMO", "/euromomo", display_euromomo),
+    #AppLink("EUROMOMO", "EUROMOMO", "/euromomo", display_euromomo),
     AppLink(lazy_gettext("Evolution per country"), lazy_gettext("Evolution per country"), "/evolution_per_country", display_a, callback_fn=callback_a),
     AppLink(lazy_gettext("Cases per million"), lazy_gettext("Cases per million"), "/cases_per_million", display_cases_per_million, callback_fn=callback_cases_per_million),
     AppLink(lazy_gettext("Deaths per million"), lazy_gettext("Deaths per million"), "/deaths_per_million", display_deaths_per_million, callback_fn=callback_deaths_per_million),
