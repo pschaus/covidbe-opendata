bd72786837b862b05ebbd820c4b5ad6b1bc3fa4e
Pierre Schaus <pschaus@gmail.com>
2020-05-12 07:46:27 +0200

    update tomtom page

--- graphs/tomtom_traffic.py
+++ graphs/tomtom_traffic.py
@@ -13,42 +13,42 @@ import datetime
 
 # Loading dataset from an url
 
-def get_belgium_df_working_days() :
 
-    belgium = ["antwerp", "brussels", "charleroi", "ghent", "kortrijk", "leuven", "liege", "mons", "namur"]
-
-    belgium_df = pd.DataFrame()
-
-    for e in belgium:
-        df = pd.read_csv("static/csv/tomtom/BEL_" + e + ".csv")
-        df['city'] = e
+def get_tomtom_df(cities):
+    eu_df = pd.DataFrame()
+    for n in cities:
+        df = pd.read_csv(f"static/csv/tomtom/{n}.csv")
+        df['city'] = n[4:]
         df['Time'] = pd.to_datetime(df['Time'])
         df.index = df['Time']
         df = df.between_time('07:00', '09:00')
         df = df[df.index.dayofweek < 5]
         df = df[df.Time.dt.date != datetime.date(2020, 5, 1)]
+        df = df[df.Time.dt.date != datetime.date(2020, 5, 8)]
         df = df[df.Time.dt.date > datetime.date(2020, 4, 14)]
         df = df.groupby([df.city, df.Time.dt.date]).agg(['mean'])
         df.columns = df.columns.droplevel(1)
-        belgium_df = belgium_df.append(df, ignore_index=False)  # Add df one by one
-    
-    return belgium_df
+        eu_df = eu_df.append(df, ignore_index=False)  # Add df one by one
+
+    return eu_df
+
+def get_belgium_df_working_days() :
+    cities_be = ["BEL_antwerp", "BEL_brussels", "BEL_charleroi", "BEL_ghent", "BEL_kortrijk", "BEL_leuven", "BEL_liege", "BEL_mons", "BEL_namur"]
+    return get_tomtom_df(cities_be)
 
 
-@register_plot_for_embedding("tomtom_traffic_working_days")
-def plot_tomtom_be_working_days():
-    """
-    tomtom traffic working days
-    """
-    
-    belgium_df = get_belgium_df_working_days()
-    
+def get_eu_df_working_days():
+    cities_neighbors = ["BEL_brussels", "ESP_barcelona", "SWE_stockholm", "FRA_paris", "DEU_berlin", "AUT_vienna",
+                        "NOR_oslo", "GBR_london", "ITA_rome", "NLD_amsterdam"]
+    return  get_tomtom_df(cities_neighbors)
+
+
+
+def plot_traffic_fig(df):
     # Plotting a Map to show real-time traffic around the world
-    # fig = px.bar(bel_bru, y='LiveTraffic', x='Time')
-    fig = px.line(belgium_df, y='LiveTraffic', x=belgium_df.index.get_level_values(1),
-                  color=belgium_df.index.get_level_values(0))
+    fig = px.line(df, y='LiveTraffic', x=df.index.get_level_values(1),
+                  color=df.index.get_level_values(0))
 
-    # fig.update_traces(texttemplate='%{text:.2s}', textposition='outside')
     fig.update_layout(uniformtext_minsize=8,
                       uniformtext_mode='hide',
                       xaxis_title="TomTom Traffic Index",
@@ -70,6 +70,25 @@ def plot_tomtom_be_working_days():
     return fig
 
 
+belgium_df = get_belgium_df_working_days()
+
+@register_plot_for_embedding("tomtom_traffic_working_days")
+def plot_tomtom_be_working_days():
+    """
+    tomtom traffic working days
+    """
+    return  plot_traffic_fig(belgium_df)
+
+
+eu_df = get_eu_df_working_days()
+
+@register_plot_for_embedding("tomtom_traffic_working_days")
+def plot_tomtom_eu_working_days():
+    """
+    tomtom traffic working days
+    """
+    return plot_traffic_fig(eu_df)
+
 # importing extra modules
 from os import listdir
 from os.path import isfile, join
@@ -89,52 +108,30 @@ def get_belgium_neighbors_df_working_days() :
     selected = placefiles[1:]
 
     for e in selected :
+        keys = e.strip().replace(".", "_").strip().split("_")
 
         df = pd.read_csv(mypath+e)
-
-        keys = e.strip().replace(".","_").strip().split("_")
-
         df['key'] = keys[0] + '_' + keys[1]
         df['country'] = keys[0].strip()+""
         df['city'] = keys[1].strip()+""
-
         df['Time'] = pd.to_datetime(df['Time'])
         df['Hour'] = df.Time.dt.hour
         df['Date'] = df.Time.dt.date
         df.index = df['Time']
-
         df = df.between_time('07:00', '09:00')
         df = df[df.index.dayofweek < 5]
         df = df[df.Time.dt.date != datetime.date(2020,5,1)]
+        df = df[df.Time.dt.date != datetime.date(2020, 5, 8)]
+
         df = df[df.Time.dt.date > datetime.date(2020,4,13)]
-
-        #df = df.groupby([df.country, df.city,  df.Date, df.Hour]).agg(['mean'])
         df = df.groupby([df.country, df.city,  df.Date]).agg(['mean'])
-
         df.columns = df.columns.droplevel(1)
 
         places_df = places_df.append(df, ignore_index=False) # Add df one by one
 
-    # Exploring the dataset
-
-    #placed_df = pd.merge(places_df, world, how='left', on=['country', 'city'], indicator='indicator_column', left_index=False, right_index=False)
-
-    #response = requests.get("https://api.midway.tomtom.com/ranking/live/")
-
-    #world = pd.json_normalize(json.loads(response.text))
-
-    #world[['country','shape','city']] = world["circle.key"].str.split("/", expand = True)
-    #world['data.UpdateTime'] = pd.to_datetime(world['data.UpdateTime'], unit = "ms")
-    #world.city = world.city.astype(str)
-    #world.country = world.country.astype(str)
-
-    #lonlat = world[['country', 'city', 'circle.shape.centerLon','circle.shape.centerLat']]
-
-    #lonlat.to_csv(mypath+"all/citieslonlat.csv", index=False)
 
     lonlat = pd.read_csv(mypath+"all/citieslonlat.csv")
 
-    #print(lonlat)
 
     lonlat.set_index(['country', 'city'], inplace=True)
     lonlat = lonlat.reset_index().drop_duplicates(subset=['country', 'city'], keep='last').set_index(['country', 'city'])
@@ -143,23 +140,18 @@ def get_belgium_neighbors_df_working_days() :
 
     places_df['country'] = places_df.index.get_level_values(0)
     places_df['city'] = places_df.index.get_level_values(1)
-    #places_df['Hour'] = places_df.index.get_level_values(2)
     places_df['Date'] = places_df.index.get_level_values(2)#(3)
-
     places_df['DateH'] = pd.to_datetime(places_df['Date'])
-    #places_df['DateH'] += pd.to_timedelta(places_df.Hour, unit='h')
     places_df.DateH = places_df.DateH.dt.strftime('%Y-%m-%d')#('%Y-%m-%d %H')
 
     return places_df
 
-
+places_df = get_belgium_neighbors_df_working_days()
 @register_plot_for_embedding("map_tomtom_be_working_days")
 def map_tomtom_be_working_days():
     """
     tomtom traffic map working days
     """
-
-    places_df = get_belgium_neighbors_df_working_days()
 
     fig = px.scatter_mapbox(places_df,
                             lat = "circle.shape.centerLat",
@@ -167,11 +159,11 @@ def map_tomtom_be_working_days():
                             color = "LiveTraffic",#"data.JamsDelay",
                             size = "LiveTraffic",
                             color_continuous_scale = px.colors.sequential.Viridis,
-                            range_color = [min(places_df.LiveTraffic),max(places_df.LiveTraffic)],
+                            range_color = [10,max(places_df.LiveTraffic)],
                             animation_frame="DateH",
                             size_max=20,
-                            zoom = 7, center = {"lat": 50.8466, "lon": 4.3528})#,
-                            #hover_name="city", hover_data=["country", "data.JamsDelay", "data.JamsCount", "data.JamsLength", "data.UpdateTime"])
+                            height=700,
+                            zoom = 4, center = {"lat": 50.8466, "lon": 4.3528})#,
 
     fig.update_layout(mapbox_style="carto-positron")
 
