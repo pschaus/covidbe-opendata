535cb8b4a7c7219190c2d4edb3a330043c7eee8a
Pierre Schaus <pschaus@gmail.com>
2020-05-13 16:03:14 +0200

    admin region page

---
+++ graphs/cases_per_admin_region.py
@@ -0,0 +1,71 @@
+from datetime import datetime, date
+
+import geopandas
+import plotly.express as px
+import pandas as pd
+import numpy as np
+from flask_babel import gettext
+
+from graphs import register_plot_for_embedding
+
+df = pd.merge(pd.read_csv("static/csv/be-covid-totcases.csv", dtype={"NIS5": str}),
+                           pd.read_csv("static/csv/ins_pop.csv", dtype={"NIS5": str}),
+                           left_on='NIS5',
+                           right_on='NIS5',
+                           how='left')
+df['NIS3'] = df.apply(lambda x: x['NIS5'][:2], axis=1)
+df3 = df.groupby([df.NIS3]).agg({'CASES': ['sum'],'POP': ['sum']}).reset_index()
+df3.columns = df3.columns.get_level_values(0)
+df3['NIS3'] = df3['NIS3'].astype(int)
+
+
+geojson = geopandas.read_file('static/json/admin-units/be-geojson.geojson')
+df_names = pd.DataFrame(geojson.drop(columns='geometry'))
+df3 = pd.merge(df3,df_names,left_on='NIS3',right_on='NIS3',how='left')
+df3['CASES/HABITANT'] = df3['CASES']/df3['POP']
+
+
+@register_plot_for_embedding("cases_per_admin_region_inhabitant")
+def map_totcases_admin_region():
+    fig = px.choropleth_mapbox(df3, geojson=geojson,
+                               locations="NIS3",
+                               color='CASES', color_continuous_scale="magma_r",
+                               featureidkey="properties.NIS3",
+                               center={"lat": 50.641111, "lon": 4.668889},
+                               hover_name="CASES",
+                               hover_data=["CASES", "name"],
+                               height=600,
+                               mapbox_style="carto-positron", zoom=6)
+    fig.update_geos(fitbounds="locations")
+    fig.layout.coloraxis.colorbar.title = gettext("Number of cases")
+    fig.layout.coloraxis.colorbar.titleside = "right"
+    fig.layout.coloraxis.colorbar.ticks = "outside"
+    fig.layout.coloraxis.colorbar.tickmode = "array"
+    fig.update_traces(
+        hovertemplate=gettext(gettext("<b>%{customdata[0]}<br><b>%{customdata[1]}"))
+    )
+    fig.update_layout(template="plotly_white", margin=dict(l=0, r=0, t=5, b=0))
+    return fig
+
+
+@register_plot_for_embedding("cases_per_habitant_admin_region_inhabitant")
+def map_cases_per_habittant_admin_region():
+    fig = px.choropleth_mapbox(df3, geojson=geojson,
+                               locations="NIS3",
+                               color='CASES/HABITANT', color_continuous_scale="magma_r",
+                               featureidkey="properties.NIS3",
+                               center={"lat": 50.641111, "lon": 4.668889},
+                               hover_name="CASES",
+                               hover_data=["CASES", "name"],
+                               height=600,
+                               mapbox_style="carto-positron", zoom=6)
+    fig.update_geos(fitbounds="locations")
+    fig.layout.coloraxis.colorbar.title = gettext("Number of cases per habitant")
+    fig.layout.coloraxis.colorbar.titleside = "right"
+    fig.layout.coloraxis.colorbar.ticks = "outside"
+    fig.layout.coloraxis.colorbar.tickmode = "array"
+    fig.update_traces(
+        hovertemplate=gettext(gettext("<b>%{customdata[0]}<br><b>%{customdata[1]}"))
+    )
+    fig.update_layout(template="plotly_white", margin=dict(l=0, r=0, t=5, b=0))
+    return fig
--- pages/cases/__init__.py
+++ pages/cases/__init__.py
@@ -2,6 +2,7 @@ from flask_babel import lazy_gettext
 
 from pages import AppMenu, AppLink
 from pages.cases.municipalities import municipalities_link
+from pages.cases.admin_region import display_admin
 from pages.cases.provinces import display_provinces
 from pages.cases.age_groups import display_age_groups
 from pages.cases.testing import display_testing
@@ -10,6 +11,7 @@ from pages.cases.Re import display_Re, callback_Re
 cases_menu = AppMenu(lazy_gettext("Cases"), "/cases", [
     municipalities_link,
     AppLink(lazy_gettext("Cases per province"), lazy_gettext("Per province"), "/provinces", display_provinces),
+    AppLink(lazy_gettext("Cases per admin region"), lazy_gettext("Per admin region"), "/admin_region", display_admin),
     AppLink(lazy_gettext("Cases per age groups"), lazy_gettext("Age Groups"), "/age_groups", display_age_groups),
     AppLink(lazy_gettext("Testing"), lazy_gettext("Testing"), "/testing", display_testing),
     AppLink(lazy_gettext("Epidemic Indicators"), lazy_gettext("Epidemic Indicators"), "/Re", display_Re, callback_fn=callback_Re)
