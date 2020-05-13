25a7afa36b0f2de539fad58515cd483f74e0e2ab
Pierre Schaus <pschaus@gmail.com>
2020-05-13 16:17:20 +0200

    update admin region page

--- pages/cases/admin_region.py
+++ pages/cases/admin_region.py
@@ -8,15 +8,16 @@ from flask_babel import get_locale, gettext
 from graphs.cases_per_admin_region import map_totcases_admin_region, map_cases_per_habittant_admin_region
 from pages.sources import *
 
+from pages import get_translation
 
 def display_admin():
     return [
-        html.H2(gettext("Total Number of cases since the beginning")),
+        html.H2(gettext(get_translation(en="Total Number of cases since the beginning", fr = "Nombre de cas depuis le début"))),
         dbc.Row([
             dbc.Col(dcc.Graph(id='cases-province-map', figure=map_totcases_admin_region(),
                               config=dict(locale=str(get_locale()))), className="col-12"),
         ]),
-        html.H2(gettext("Total Number of cases per inhabitant")),
+        html.H2(gettext(get_translation(en="Total Number of cases per 1000 inhabitant", fr ="Nombre de cas par 1000 habitants "))),
         dbc.Row([
             dbc.Col(dcc.Graph(id='cases-province-map', figure=map_cases_per_habittant_admin_region(),
                               config=dict(locale=str(get_locale()))), className="col-12"),
