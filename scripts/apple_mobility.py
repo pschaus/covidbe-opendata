3fff8fa20d21621082d07b880986a55ab6c5896d
Pierre Schaus <pschaus@gmail.com>
2020-05-21 11:41:24 +0200

    update data

--- scripts/apple_mobility.py
+++ scripts/apple_mobility.py
@@ -12,7 +12,7 @@ from datetime import timedelta
 
 
 import urllib.request, json
-with urllib.request.urlopen("https://covid19-static.cdn-apple.com/covid19-mobility-data/current/v2/index.json") as url:
+with urllib.request.urlopen("https://covid19-static.cdn-apple.com/covid19-mobility-data/current/v3/index.json") as url:
     data = json.loads(url.read().decode())
 
     DOWNLOAD_URL = "https://covid19-static.cdn-apple.com"+data['basePath']+data['regions']['en-us']['csvPath']
