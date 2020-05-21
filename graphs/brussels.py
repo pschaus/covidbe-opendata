1b011de20cb72434cd7aac2a78847d18688ad2f1
Pierre Schaus <pschaus@gmail.com>
2020-05-21 11:43:11 +0200

    update tunnels

--- graphs/brussels.py
+++ graphs/brussels.py
@@ -9,24 +9,24 @@ from flask_babel import gettext
 from datetime import datetime
 import plotly.express as px
 
+from graphs import register_plot_for_embedding
 
+import pandas as pd
+import plotly.graph_objs as go
+from flask_babel import gettext
+
+from datetime import datetime
+import plotly.express as px
 
 mydateparser = lambda x: datetime.strptime(x, "%d/%m/%Y %H:%M")
 
-df = pd.read_csv(f'static/csv/tunnels_bxl.csv',parse_dates = ['from','to'],date_parser=mydateparser)
-
-
+df = pd.read_csv(f'static/csv/tunnels_bxl.csv', parse_dates=['from', 'to'], date_parser=mydateparser)
 
 mask = (df['from'] >= '2020-02-1')
 df = df.loc[mask]
 
 mask = (df['from'] <= '2020-05-19')
 df = df.loc[mask]
-
-
-df.sort_values(by='from',inplace=True)
-
-
 
 df.reset_index(level=0, inplace=True)
 df.set_index('from', inplace=True)
@@ -75,7 +75,32 @@ def fig_tunnel_ratio(weekday=True, time='07:00'):
     return fig
 
 
+def fig_global_tunnel_ratio(weekday=True, time='07:00'):
+    df_ = df.between_time(time, time)
+    if weekday:
+        df_ = df_[df_.index.dayofweek < 5]
+    else:
+        df_ = df_[df_.index.dayofweek == 5]
+    df_.reset_index(level=0, inplace=True)
 
+    df_day = df_.groupby([df_['from'].dt.date]).agg({'volume': ['sum']}).reset_index()
+    df_day.columns = df_day.columns.get_level_values(0)
+
+    df_day.reset_index(level=0, inplace=True)
+    df_day['from'] = pd.to_datetime(df_day['from'])
+    mask = (df_day['from'] <= '2020-03-10')
+
+    df2 = df_day.loc[mask]
+    mean = df2['volume'].mean()
+
+    df_day['rel_volume'] = df_day.volume / mean
+    fig = px.line(df_day, x="from", y="rel_volume")
+
+    fig.update_layout(
+        xaxis_title="day",
+        yaxis_title="ratio wrt to normality"
+    )
+    return fig
 
 
 @register_plot_for_embedding("brussels-tunnels")
@@ -90,3 +115,12 @@ def brussels_tunnels_ratio():
     fig1 = fig_tunnel_ratio(True, '07:00')
     fig1.update_layout(title="#Tunnel Traffic of Brussels at 7:00-8:00 Workdays")
     return fig1
+
+
+@register_plot_for_embedding("brussels-tunnels-ratio")
+def brussels_alltunnels_ratio():
+    fig1 = fig_global_tunnel_ratio(True, '07:00')
+    fig1.update_layout(title="#Total Tunnel Traffic of Brussels at 7:00-8:00 Workdays")
+    return fig1
+
+
--- pages/lockdown/brussels.py
+++ pages/lockdown/brussels.py
@@ -4,7 +4,7 @@ import dash_core_components as dcc
 import dash_html_components as html
 from flask_babel import get_locale, gettext
 
-from graphs.brussels import brussels_tunnels, brussels_tunnels_ratio
+from graphs.brussels import brussels_tunnels, brussels_tunnels_ratio, brussels_alltunnels_ratio
 from pages.sources import display_source_providers, source_google_traffic, source_brussels_mobility
 from pages import get_translation
 
@@ -20,6 +20,12 @@ def display_brussels():
         dbc.Row([
             dbc.Col(dcc.Graph(id='brussels-tunnel-ratio',
                               figure=brussels_tunnels_ratio(),
+                              config=dict(locale=str(get_locale())))),
+        ]),
+        html.H2(gettext("Brussels all tunnels relative traffic working days 7:00-8:00")),
+        dbc.Row([
+            dbc.Col(dcc.Graph(id='brussels-tunnel-ratio',
+                              figure=brussels_alltunnels_ratio(),
                               config=dict(locale=str(get_locale())))),
         ]),
         display_source_providers(source_brussels_mobility),
