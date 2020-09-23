fa41b9541579b006b8daaffc3cf107ad058ace0b
Pierre Schaus <pschaus@gmail.com>
2020-09-23 09:27:22 +0200

    update layout hospi prov

--- pages/hospitals/hospitals_prov.py
+++ pages/hospitals/hospitals_prov.py
@@ -16,6 +16,7 @@ from pages.sources import display_source_providers, source_sciensano
 def display_hospitals_prov():
     return [
         html.H2(gettext("Total hospitalizations per province")),
+        html.H3(gettext("Absolute numbers")),
         dbc.Row([
             dbc.Col(dcc.Graph(id='hospitalization-prov-map',
                               figure=map_hospi_provinces(),
@@ -27,6 +28,12 @@ def display_hospitals_prov():
                               config=dict(locale=str(get_locale())))),
         ]),
         dbc.Row([
+            dbc.Col(dcc.Graph(id='hospitalization-prov',
+                              figure=total_hospi_provinces(),
+                              config=dict(locale=str(get_locale())))),
+        ]),
+        html.H3(gettext("Per 100K inhabitants")),
+        dbc.Row([
             dbc.Col(dcc.Graph(id='hospitalization100K-prov-map',
                               figure=map_hospi_per100K_provinces(),
                               config=dict(locale=str(get_locale())))),
@@ -36,33 +43,22 @@ def display_hospitals_prov():
                               figure=scatter_hospi_per100K_provinces(),
                               config=dict(locale=str(get_locale())))),
         ]),
-        dbc.Row([
-            dbc.Col(dcc.Graph(id='hospitalization-prov',
-                              figure=total_hospi_provinces(),
-                              config=dict(locale=str(get_locale())))),
-        ]),
-        html.H2(gettext("Total ICU per province")),
+        html.H3(gettext("Total ICU per province")),
         dbc.Row([
             dbc.Col(dcc.Graph(id='hospitalization-prov',
                               figure=total_icu_provinces(),
                               config=dict(locale=str(get_locale())))),
         ]),
-        html.H2(gettext("Total daily new hospitalizations per province")),
+        html.H3(gettext("Total daily new hospitalizations per province")),
         dbc.Row([
             dbc.Col(dcc.Graph(id='hospitalization-prov',
                               figure=total_hospi_new_in_provinces(),
                               config=dict(locale=str(get_locale())))),
         ]),
-        html.H2(gettext("Total daily persons out of hospital per province")),
+        html.H3(gettext("Total daily persons out of hospital per province")),
         dbc.Row([
             dbc.Col(dcc.Graph(id='hospitalization-prov',
                               figure=total_hospi_new_out_provinces(),
-                              config=dict(locale=str(get_locale())))),
-        ]),
-        html.H2(gettext("Hospitalizations per province")),
-        dbc.Row([
-            dbc.Col(dcc.Graph(id='hospitalization-prov',
-                              figure=bar_hospi_per_case_per_province(),
                               config=dict(locale=str(get_locale())))),
         ]),
         display_source_providers(source_sciensano)
