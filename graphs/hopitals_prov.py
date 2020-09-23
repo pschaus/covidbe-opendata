0c970b880112eb144144fd33ed8b972960b3479f
Pierre Schaus <pschaus@gmail.com>
2020-09-23 09:17:07 +0200

    new map provinces hospitalizations

--- graphs/hopitals_prov.py
+++ graphs/hopitals_prov.py
@@ -19,6 +19,54 @@ range_max = df_prov_tot.CASES_PER_THOUSAND.max()
 
 df = pd.read_csv('static/csv/be-covid-provinces-all.csv')
 
+dates = df.groupby([df.PROVINCE,df.PROV]).agg({'DATE': ['max']}).reset_index()
+dates.columns = dates.columns.get_level_values(0)
+dfl = dates.merge(df, how='left', left_on=['DATE','PROV','PROVINCE'],right_on=['DATE','PROV','PROVINCE'])
+dfl['HOSPI_PER_100000'] = (dfl.TOTAL_IN/dfl.POP)*100000
+
+
+def map_hospi(column,title):
+    fig = px.choropleth_mapbox(dfl, geojson=geojson_provinces,
+                               locations="PROV",
+                               color=column,
+                               color_continuous_scale="magma_r",
+                               featureidkey="properties.proviso",
+                               center={"lat": 50.641111, "lon": 4.668889},
+                               hover_name=column,
+                               hover_data=[column, "POP", "PROVINCE"],
+                               height=600,
+                               mapbox_style="carto-positron", zoom=6)
+    fig.update_geos(fitbounds="locations")
+    fig.layout.coloraxis.colorbar.title = get_translation(fr=title,
+                                                          en=title)
+    fig.layout.coloraxis.colorbar.titleside = "right"
+    fig.layout.coloraxis.colorbar.ticks = "outside"
+    fig.layout.coloraxis.colorbar.tickmode = "array"
+    fig.update_traces(
+        hovertemplate=gettext(
+            gettext(title+":<b>%{customdata[0]}<br>pop:<b>%{customdata[1]}<br><b>%{customdata[2]}"))
+    )
+    fig.update_layout(template="plotly_white", margin=dict(l=0, r=0, t=5, b=0))
+    return fig
+
+def scatter_hospi(column,title):
+    dfls = dfl.sort_values(by=column, axis=0)
+    fig = px.scatter(y=dfls.PROVINCE, x=dfls[column],title=title)
+    return fig
+
+
+def map_hospi_provinces():
+    return map_hospi('TOTAL_IN','Total Hospitalizations')
+
+def scatter_hospi_provinces():
+    return scatter_hospi('TOTAL_IN','Total Hospitalizations')
+
+def map_hospi_per100K_provinces():
+    return map_hospi('HOSPI_PER_100000','Total Hospitalizations per 100K inhabitants')
+
+def scatter_hospi_per100K_provinces():
+    return scatter_hospi('HOSPI_PER_100000','Total Hospitalizations per 100K inhabitants')
+
 
 def total_hospi_provinces():
     return px.line(df, x="DATE", y="TOTAL_IN", color="PROVINCE")
--- pages/hospitals/hospitals_prov.py
+++ pages/hospitals/hospitals_prov.py
@@ -3,7 +3,12 @@ import dash_bootstrap_components as dbc
 import dash_core_components as dcc
 import dash_html_components as html
 from flask_babel import get_locale, gettext
-from graphs.hopitals_prov import total_hospi_new_in_provinces,total_hospi_new_out_provinces,total_hospi_provinces,total_icu_provinces
+from graphs.hopitals_prov import total_hospi_new_in_provinces,\
+    total_hospi_new_out_provinces,\
+    total_hospi_provinces,total_icu_provinces,\
+    map_hospi_provinces,map_hospi_per100K_provinces,\
+    scatter_hospi_provinces,\
+    scatter_hospi_per100K_provinces
 from graphs.hopitals import bar_hospi_per_case_per_province
 from pages.sources import display_source_providers, source_sciensano
 
@@ -11,6 +16,26 @@ from pages.sources import display_source_providers, source_sciensano
 def display_hospitals_prov():
     return [
         html.H2(gettext("Total hospitalizations per province")),
+        dbc.Row([
+            dbc.Col(dcc.Graph(id='hospitalization-prov-map',
+                              figure=map_hospi_provinces(),
+                              config=dict(locale=str(get_locale())))),
+        ]),
+        dbc.Row([
+            dbc.Col(dcc.Graph(id='hospitalization-prov-scatter',
+                              figure=scatter_hospi_provinces(),
+                              config=dict(locale=str(get_locale())))),
+        ]),
+        dbc.Row([
+            dbc.Col(dcc.Graph(id='hospitalization100K-prov-map',
+                              figure=map_hospi_per100K_provinces(),
+                              config=dict(locale=str(get_locale())))),
+        ]),
+        dbc.Row([
+            dbc.Col(dcc.Graph(id='hospitalization100K-prov-scatter',
+                              figure=scatter_hospi_per100K_provinces(),
+                              config=dict(locale=str(get_locale())))),
+        ]),
         dbc.Row([
             dbc.Col(dcc.Graph(id='hospitalization-prov',
                               figure=total_hospi_provinces(),
