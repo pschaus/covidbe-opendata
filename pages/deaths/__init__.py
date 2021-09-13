4f49fb3e44ca8214a0766fbb1701550ecb9a9ee5
Pierre Schaus <pschaus@gmail.com>
2021-09-13 15:55:27 +0200

    auto-commit from pschaus@wifi-secure2-952 on Mon Sep 13 15:55:23 CEST 2021

--- pages/deaths/__init__.py
+++ pages/deaths/__init__.py
@@ -6,7 +6,6 @@ from pages import AppMenu, AppLink
 
 from pages.deaths.overmortality import display_overmortality
 from pages.deaths.age_groups import display_age_groups
-from pages.deaths.obituary import display_obituary
 from pages.deaths.covid_deaths import display_covid_death
 
 
@@ -18,7 +17,6 @@ deaths_menu = AppMenu(lazy_gettext("Deaths"), "/deaths", [
             lazy_gettext("Covid Deaths"),
             "/covid", display_covid_death),
     AppLink(get_translation(fr="Décès totaux / Groupes d'ages", en="Overal Deaths / Age group"), lazy_gettext("All Deaths"), "/all", display_age_groups),
-    AppLink(get_translation(fr= "Evolution Nécrologiques", en = "Obituary evolution"), get_translation(fr= "Nécrologie", en = "Obituary"), "/obituary", display_obituary),
 
 ])
 
--- pages/lockdown/__init__.py
+++ pages/lockdown/__init__.py
@@ -19,12 +19,5 @@ def call_back_fb_lux(app):
 lockdown_menu = AppMenu(lazy_gettext("Lockdown Impact"), "/lockdown", [
     AppLink(lazy_gettext("Mobility Reports"), lazy_gettext("Mobility Report"), "/mobility-reports", display_mobility),
     AppLink(lazy_gettext("Google-Map Traffic"), lazy_gettext("Google Map Internet Traffic EU"), "/google-map", display_traffic),
-    AppLink(lazy_gettext("TomTom Traffic Data"), lazy_gettext("TomTom Traffic Data"), "/tomtom", display_tomtom),
-    AppLink(lazy_gettext("Brussels"), lazy_gettext("Brussels"), "/brussels", display_brussels),
-    AppLink(lazy_gettext("Facebook"), lazy_gettext("Facebook"), "/facebook", display_facebook, callback_fn=callback_fb),
-    AppLink(lazy_gettext("Facebook-lux"), lazy_gettext("Facebook-lux"), "/facebook_lux", display_facebook_lux, callback_fn=call_back_fb_lux,invisible=True),
-    AppLink(lazy_gettext("Facebook-eu"), lazy_gettext("Facebook-eu"), "/facebook_eu", display_facebook_eu,
-            callback_fn=call_back_fb_eu, invisible=True),
-
 ])
 
